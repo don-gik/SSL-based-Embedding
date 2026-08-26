106895dc53e872977f1b70516578b8960e5bb6ed
don-gik <224256443+don-gik@users.noreply.github.com>
2026-08-26 16:26:29 +0900

    ssl added

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import ssl
+
+try:
+    _create_unverified_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_context
+
 import logging
 
 import hydra
